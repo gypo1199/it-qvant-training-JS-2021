5bc34a770819176fb84a66c2be807c28be5a71f5
ArtemP <gypo1199@gmail.com>
2021-07-08 15:00:58 +0700

    init commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Привет, Vue!',
+
+      images:[
+        {
+          url:'https://www.google.ru/',
+          title:'Google',
+          alt:'Google.jpg.jpg',
+          img:'../img/google.jpg',
+          discription:'Поисковая система google',
+        },
+        {
+          url:'https://yandex.ru/',
+          title:'Yandex',
+          alt:'Yandex.jpg',
+          img:'../img/yandex.jpg',
+          discription:'Поисковая система Yandex',
+        },
+      ]
+    },
+     
+  })
